9fd4bf4f8dc3655262fe959b82d5d440d2a4e503
rahman_z05 <mailtorahman.z@gmail.com>
2021-10-14 21:01:42 +0530

    added route to item's title

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router";
 import CollectionItem from "../collection-item/CollectionItem";
 import "./preview.scss";
 
 function Preview(props) {
   const { items, title } = props.item;
+  const history = useHistory();
   return (
     <div className="collectionPreview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <h1
+        className="title"
+        onClick={() =>
+          history.push(`${history.location.pathname}/${title.toLowerCase()}`)
+        }
+      >
+        {" "}
+        {title.toUpperCase()}
+      </h1>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
